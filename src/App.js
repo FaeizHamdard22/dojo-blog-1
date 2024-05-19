e7a3cc1da06ddef1b367c256d3d96e9eff814247
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-19 10:50:52 +0430

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-
+import Navbar from "./Navbar";
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 150;
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{[1, 2, 3, 4]}</p>
-        <p>{Math.random() * 10}</p>
-      </div>
+      <Navbar />
+      <h1>App Component</h1>
     </div>
   );
 }
