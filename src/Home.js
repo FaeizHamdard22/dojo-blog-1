ae118a634057103dc28335560f099c348176b8bc
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-20 10:33:16 +0430

    Delete button added.

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,18 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     {title: 'My new Website', body: "lorem ipsum...", author: "mario", id: 1},
     {title: 'Welcome party!', body: "lorem ipsum...", author: "luigi", id: 2},
-    {title: 'Web dev top tips', body: "lorem ipsum...", author: "yoshi", id: 3},
+    {title: 'Web dev top tips', body: "lorem ipsum...", author: "mario", id: 3},
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
       <h2 className='home-title'>Home Page</h2>
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
