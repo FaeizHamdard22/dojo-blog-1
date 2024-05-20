80e52efaa18a9dfd381fd04a804c23e57076ce93
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-20 11:10:19 +0430

    data folder with db.json file added and also json-server package added to the project.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,15 +8,24 @@ const Home = () => {
     {title: 'Web dev top tips', body: "lorem ipsum...", author: "mario", id: 3},
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log("useEffect ran");
+    console.log(blogs);
+  }, [name]);
+
   return (
     <div className="home">
       <h2 className='home-title'>Home Page</h2>
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 };
