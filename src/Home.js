6701ee91cb0eefcac068c94f0971a0f1ad6e8f31
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-21 18:59:31 +0430

    using fetch API.

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: 'My new Website', body: "lorem ipsum...", author: "mario", id: 1},
-    {title: 'Welcome party!', body: "lorem ipsum...", author: "luigi", id: 2},
-    {title: 'Web dev top tips', body: "lorem ipsum...", author: "mario", id: 3},
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState("mario");
 
@@ -16,16 +12,19 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log("useEffect ran");
-    console.log(blogs);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, []);
 
   return (
     <div className="home">
       <h2 className='home-title'>Home Page</h2>
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>Change Name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
     </div>
   );
 };
