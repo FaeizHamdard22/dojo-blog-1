f1f014fd221be9e9c9291c6218088e18e3e00e02
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-25 20:01:17 +0430

    Search box created

---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+const SearchBox = () => {
+  return (
+    <div className="search">
+      <input
+        type="search"
+        placeholder="Search"
+        className="bg-light form-control"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
